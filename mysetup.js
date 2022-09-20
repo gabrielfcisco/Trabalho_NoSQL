9d6769e03481294d39b1ebc4cc1e92f27fe368d2
Gabriel Sousa <gabrifran017@gmail.com>
2022-09-20 15:22:22 -0300

    Update mysetup.js

--- mysetup.js
+++ mysetup.js
@@ -1,18 +1,33 @@
 let error = true
 
-let res = [
+let create_alunos = [
   db.createCollection('alunos'),
   db.alunos.createIndex({ RA: 1 }, { unique: true }),
   db.alunos.createIndex({ Nome: 1 }),
-  db.alunos.insert({ RA: '265974164', Nome: 'Gabriel' }),
-  db.alunos.insert({ RA: '165654646', Nome: 'João' }),
-  db.alunos.insert({ RA: '165465644', Nome: 'Luís' }),
-  db.alunos.insert({ RA: '564564654', Nome: 'João' })
+  db.alunos.createIndex({ Sobrenome: 1 }),
+  db.alunos.insert({ RA: '265974164', Nome: 'Gabriel', Sobrenome:'Sousa' }),
+  db.alunos.insert({ RA: '165654646', Nome: 'João', Sobrenome:'Ferreira' }),
+  db.alunos.insert({ RA: '165465644', Nome: 'Luís', Sobrenome:'Costa' }),
+  db.alunos.insert({ RA: '564564654', Nome: 'João', Sobrenome:'Vasconcelos' })
 ]
 
 printjson(res)
 
-if (error) {
-  print('Error, exiting')
-  quit(1)
-}
+let create_professores = [
+  db.createCollection('professores'),
+  db.professores.createIndex({ RP : 1 }, { unique: true }),
+  db.professores.createIndex({ Nome : 1 }),
+  db.professores.createIndex({ Sobrenome : 1 }),
+  db.professores.insert({ RP: '22222222', Nome: 'Jair', Sobrenome:'Bolsonaro' }),
+  db.professores.insert({ RP: '13131313', Nome: 'Lula', Sobrenome:'da Silva' }),
+]
+
+printjson(create_professores)
+
+let create_materias = [
+  db.createCollection('materias'),
+  db.materias.createIndex({ ID : 1 }, { unique: true }),
+  db.materias.createIndex({ Nome : 1 }),
+  db.materias.createIndex({ Créditos : 1 }),
+  db.materias.insert({ ID: '69893826', Nome: 'DSW', Créditos:'06' }),
+]
