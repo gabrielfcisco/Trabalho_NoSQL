82195fd7ce0dd39cc48bb7952e3f54708c734e60
JavihFeh <37805872+JavihFeh@users.noreply.github.com>
2022-09-22 20:04:52 -0300

    v2joaoF
    mudei o RA

--- mysetup.js
+++ mysetup.js
@@ -5,7 +5,7 @@ let create_alunos = [
   db.alunos.createIndex({ Nome: 1 }),
   db.alunos.createIndex({ Sobrenome: 1 }),
   db.alunos.insert({ RA: '265974164', Nome: 'Gabriel', Sobrenome:'Sousa' }),
-  db.alunos.insert({ RA: '165654646', Nome: 'João Vitor', Sobrenome:'Ferreira dos Santos' }),
+  db.alunos.insert({ RA: '22896419', Nome: 'João Vitor', Sobrenome:'Ferreira dos Santos' }),
   db.alunos.insert({ RA: '165465644', Nome: 'Luís', Sobrenome:'Costa' }),
   db.alunos.insert({ RA: '564564654', Nome: 'João', Sobrenome:'Vasconcelos' })
 ]
